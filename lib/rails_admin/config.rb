932060969bf7ee01e4d70a3b3e6a779fa2c8d67d
Masatsugu Shimojo <shimojo.masatsugu@gmail.com>
2019-12-22 13:32:28 +0900

    [fix]Rails -> Jets

--- lib/rails_admin/config.rb
+++ lib/rails_admin/config.rb
@@ -337,7 +337,7 @@ module RailsAdmin
       def viable_models
         included_models.collect(&:to_s).presence || begin
           @@system_models ||= # memoization for tests
-            ([Jets.application] + Rails::Engine.subclasses.collect(&:instance)).flat_map do |app|
+            ([Jets.application] + Jets::Engine.subclasses.collect(&:instance)).flat_map do |app|
               (app.paths['app/models'].to_a + app.paths.eager_load).collect do |load_path|
                 Dir.glob(app.root.join(load_path)).collect do |load_dir|
                   Dir.glob(load_dir + '/**/*.rb').collect do |filename|
